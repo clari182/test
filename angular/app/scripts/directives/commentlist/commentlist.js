0aec8ef61bf8e35a0ec8e128f59ac613cb903e1f
Clara Youdale <clarayoudale@outlook.com>
2016-05-29 11:41:22 -0300

    Add constants to time variants

--- angular/app/scripts/directives/commentlist/commentlist.js
+++ angular/app/scripts/directives/commentlist/commentlist.js
@@ -24,8 +24,18 @@ var app = angular.module('commentList', ['comment'])
     };
   });
 
-app.controller('TimeController', ['$scope', function($scope){
+app.constant('ENUMS',
+    {
+        TimeText: { MINUTE: '1 minute', MINUTES: ' minutes', HOUR: ' hour', HOURS: ' hours', DAY: ' day', DAYS: ' days' },
+        TimeStatus: {AGO: ' ago'},
+        TimeValues: { MINUTES: 120, HOURS: 3540, DAYS: 86400 }
+    });
+
+app.controller('TimeController', ['$scope', 'ENUMS', function($scope, enums){
   $scope.calculateTimeLapse = function(value) {
+
+    if (!value) return '';
+
     var currentDate = new Date();
     var commentDate = new Date(value);  
 
@@ -33,28 +43,26 @@ app.controller('TimeController', ['$scope', function($scope){
     var returnstr = '';
 
     switch(true){
-      case (diff < 59):
-        returnstr = '1 minute ago';
+      case (diff < enums.TimeValues.MINUTES):
+        returnstr = enums.TimeText.MINUTE + enums.TimeStatus.AGO;
         break;
 
-      case (diff >= 59 && diff < 3540):
-        diff = diff / 60;
-        returnstr = diff + ' minutes ago';
+      case (diff >= enums.TimeValues.MINUTES && diff < enums.TimeValues.HOURS):
+        diff = Math.floor(diff / 60);
+        returnstr = diff + enums.TimeText.MINUTES + enums.TimeStatus.AGO;
         break;
 
-      case (diff >= 3540 && diff < 86400):
-        diff = diff / 3600;
-        returnstr = diff + ' hours ago';
+      case (diff >= enums.TimeValues.HOURS && diff < enums.TimeValues.DAYS):
+        diff = Math.floor(diff / 3600);
+        returnstr = diff + (diff < 2 ? enums.TimeText.HOUR : enums.TimeText.HOURS) + enums.TimeStatus.AGO;
         break;
 
-      case (diff == '31660026'):
-        alert('HOLA');
+      case (diff >= enums.TimeValues.DAYS):
+        diff = Math.floor(diff / enums.TimeValues.DAYS);
+        returnstr = diff + (diff < 2 ? enums.TimeText.DAY : enums.TimeText.DAYS) + enums.TimeStatus.AGO;
         break;
       
     }
-    if (diff / 1000 < 59) {
-      
-    } 
 
     return returnstr;
 
